e2c13473d8c550d1d86e4af43bf2278c982130fd
MOSQUEDACORDOVA <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2021-10-28 08:47:04 -0500

    JOSUEE MAIL

---
+++ email.php
@@ -0,0 +1,17 @@
+
+ <?php
+     if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+
+      // Set your email address where you want to receive emails. 
+       $to = 'donsteran@gmail.com';
+       $subject = 'Solicitud de contacto desde el sitio web';
+       $headers = "From: ".$name." <".$email."> \r\n";
+       $message = "Nombre: ".$name."\r\n Telefono: ".$phone."\r\n Correo: ".$email;
+       $send_email = mail($to,$subject,$message,$headers);
+
+       echo ($send_email) ? 'success' : 'error';
+
+  }?>
